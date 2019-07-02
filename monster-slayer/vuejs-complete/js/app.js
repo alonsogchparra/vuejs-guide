364ce7f80f76f35bb55134625422e2044e012ee1
Alonso Parra <alonsogparra@gmail.com>
2019-07-02 17:19:12 -0400

    Building the vuejs structure for the project

---
+++ monster-slayer/vuejs-complete/js/app.js
@@ -0,0 +1,32 @@
+new Vue({
+  el: '#app',
+  data: {
+    playerHealth: 100,
+    monsterHealth: 100,
+    gameIsRunning: false,
+
+  },
+  methods: {
+    attack: function() {
+
+    },
+    specialAttack: function() {
+
+    },
+    heal: function() {
+
+    },
+    giveUp: function() {
+
+    },
+    monsterAttacks: function() {
+
+    },
+    calculateDamage: function() {
+
+    },
+    checkWin: function() {
+
+    }
+  }
+})
