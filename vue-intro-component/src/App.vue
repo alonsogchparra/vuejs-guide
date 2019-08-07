794d4226a1fc3b4549e3636c8a31febf2e28fab6
Alonso Parra <alonsogparra@gmail.com>
2019-08-07 16:32:43 -0400

    App updated with the components

--- vue-intro-component/src/App.vue
+++ vue-intro-component/src/App.vue
@@ -1,60 +1,33 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+  <div class="container">
+    <app-header></app-header>
+    <hr>
+    <div class="row">
+      <servers></servers>
+      <app-server-details></app-server-details>
+    </div>
+    <hr>
+    <app-footer></app-footer>
   </div>
 </template>
 
 <script>
+
+import Header from './components/Shared/Header';
+import Footer from './components/Shared/Footer';
+import Servers from './components/Server/Servers';
+import ServerDetails from './components/Server/ServerDetails';
+
 export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
+  components: {
+    appHeader: Header,
+    Servers,
+    'app-server-details': ServerDetails,
+    'app-footer': Footer
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
