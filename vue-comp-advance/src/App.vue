02cd3347fa63eeb6fc023c6af4fd731b6344907b
Alonso Parra <alonsogparra@gmail.com>
2019-09-26 22:12:00 -0400

    App updated adding commponents like Quote

--- vue-comp-advance/src/App.vue
+++ vue-comp-advance/src/App.vue
@@ -1,10 +1,44 @@
 <template>
-
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <div class="d-flex py-3">
+          <div class="col-4">
+            <button class="btn btn-danger btn-block">Quote</button>
+          </div>
+          <div class="col-4">
+            <button class="btn btn-danger btn-block">Author</button>
+          </div>
+          <div class="col-4">
+            <button class="btn btn-danger btn-block">New</button>
+          </div>
+        </div>
+        <hr>
+        <p>{{ selectedComponent }}</p>
+        <app-quote>
+          <h2 slot="title">{{ quoteTitle }}</h2>
+        </app-quote>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
 
+import Quote from './components/Quote';
+import Author from './components/Author';
+import New from './components/New';
+
+export default {
+  data: function() {
+    return {
+      quoteTitle: 'The Quote',
+      selectedComponent: 'appQuote'
+    }
+  },
+  components: {
+    appQuote: Quote
+  }
 }
 </script>
 
